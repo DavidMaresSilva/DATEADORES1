4453c83b74693666ec7829ef88ffa398ce81dd4f
DavidMaresSilva <informaticasidss@611EBMJ01VDEH.salud.gob.mx>
2017-03-10 11:38:53 -0600

    inicio de prueba compu2 dateadores1 django1

---
+++ dateadores1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\informaticasidss\devops\dateadores1\dateadores1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
